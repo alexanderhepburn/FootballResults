e85683a6be84b716e228cb1401c2ccc0788765a7
alexanderhepburn <zander@hepburn.com>
2024-05-17 15:04:55 +0200

    pip bug fix

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from command_manager import command_manager
-from network_manager import network_manager
 import subprocess
 import sys
 
 if __name__ == '__main__':
     subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+    from command_manager import command_manager
+
     command_manager().run_program()
