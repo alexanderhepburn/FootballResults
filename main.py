584438086553c6bc2ca9e90ce5b72fa3a4a7bee1
alexanderhepburn <zander@hepburn.com>
2024-05-17 15:19:09 +0200

    message supressing

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import sys
 import os
 
 if __name__ == '__main__':
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"], stdout=subprocess.DEVNULL,
+                          stderr=subprocess.DEVNULL)
     os.makedirs("tmp", exist_ok=True)
     os.makedirs("exports", exist_ok=True)
     from command_manager import command_manager
