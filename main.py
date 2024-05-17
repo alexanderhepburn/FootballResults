a3e29ae2ddcddbef7431710a7850263e8cc4de13
alexanderhepburn <zander@hepburn.com>
2024-05-17 15:02:49 +0200

    pip auto download

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from command_manager import command_manager
 from network_manager import network_manager
+import subprocess
+import sys
 
 if __name__ == '__main__':
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
     command_manager().run_program()
