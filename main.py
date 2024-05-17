8effc469c14d3ddbe15e35cafa73e1432ecf253f
alexanderhepburn <zander@hepburn.com>
2024-05-17 15:13:57 +0200

    another bug fix

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import subprocess
 import sys
+import os
 
 if __name__ == '__main__':
     subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
+    os.makedirs("tmp", exist_ok=True)
+    os.makedirs("exports", exist_ok=True)
     from command_manager import command_manager
 
     command_manager().run_program()
